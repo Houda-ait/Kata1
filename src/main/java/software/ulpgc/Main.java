c9d3476784d469c9008a282abf8deabfb604bcd7
houda <Houdaaitabdeslam@gmail.com>
2024-10-19 19:42:50 +0100

    version 1

---
+++ src/main/java/software/ulpgc/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("juan", LocalDate.of(2003,2,3)));
+    }
+}
